c0a334097095f21a59c30a811f5d07adbaa6be49
prashant <prashantverma817381@gmail.com>
2020-01-28 02:39:47 +0530

    version 1.1

--- Mrtracker/app/src/main/java/com/sunbeam/mrtracker/activity/HomeActivity.java
+++ Mrtracker/app/src/main/java/com/sunbeam/mrtracker/activity/HomeActivity.java
@@ -1,16 +1,30 @@
 package com.sunbeam.mrtracker.activity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.strictmode.NonSdkApiUsedViolation;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -19,11 +33,26 @@ import com.koushikdutta.ion.Ion;
 import com.sunbeam.mrtracker.R;
 import com.sunbeam.mrtracker.adapter.ProductAdapter;
 import com.sunbeam.mrtracker.model.Product;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
 
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private CarouselView carouselView;
+    private EditText edittext;
+
+    private int[] imageArray = new int[]{
+            R.drawable.c, R.drawable.b, R.drawable.d, R.drawable.a
+    };
 
     RecyclerView recyclerView;
     ProductAdapter adapter;
@@ -33,6 +62,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        // for side showing image
+
+        carouselView = findViewById(R.id.carousel);
+        carouselView.setPageCount(imageArray.length);
+        carouselView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                    imageView.setImageResource(imageArray[position]);
+            }
+        });
+        carouselView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+
+            }
+        });
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerXml);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
 
         recyclerView = findViewById(R.id.recyclerview);
 
@@ -45,8 +103,13 @@ public class HomeActivity extends AppCompatActivity {
         ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("blue")));
 
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_dehaze);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayShowHomeEnabled(true);
+
+
+        edittext = findViewById(R.id.search);
+
+        setEdittext(edittext);
 
 
     }
@@ -99,6 +162,11 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
+        menu.add("My Oredrs");
+        menu.add("My Cart");
+        menu.add("Clear History");
+        menu.add("LogIn");
+
         inflater.inflate(R.menu.home_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
@@ -106,16 +174,85 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.menuShopping){
-
+            Toast.makeText(this,"cart",Toast.LENGTH_SHORT).show();
         }
         else if(item.getItemId() == R.id.menuNotification){
-
+            Toast.makeText(this,"notifucation",Toast.LENGTH_SHORT).show();
         }
-        else if(item.getItemId() == R.id.menuPerson){
 
+        else if(toggle.onOptionsItemSelected(item)){
+            return true;
         }
+
+        else if(item.getTitle().equals("My Oredrs")){
+            Toast.makeText(this,"My orders",Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getTitle().equals("My Cart")){
+            Toast.makeText(this,"My cart",Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getTitle().equals("Clear History")){
+            Toast.makeText(this,"Clear history",Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getTitle().equals("LogIn")){
+            Toast.makeText(this,"Login",Toast.LENGTH_SHORT).show();
+        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.home1){
+
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+        else if(item.getItemId() == R.id.allopathic){
+            Intent intent = new Intent(this,Allopathic.class);
+            startActivity(intent);
+
+        }
+        else if(item.getItemId() == R.id.homopathic){
+            Intent intent = new Intent(this,Homoeopathy.class);
+            startActivity(intent);
+        }
+        else if(item.getItemId() == R.id.ayurvedic){
+            Intent intent = new Intent(this,Ayurvedic.class);
+            startActivity(intent);
+        }
+
+        return false;
+    }
+
+
+    //for searching lisner
+    public void setEdittext(final EditText edittext) {
+        this.edittext = edittext;
+        edittext.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    String search = edittext.getText().toString();
+                   // Log.e("HomeActivity", search);
+                    performSearch(search);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private void performSearch(String search) {
+        Intent intent = new Intent(this,Search.class);
+        intent.putExtra("search",search);
+        startActivity(intent);
+
     }
 
 
---
+++ Mrtracker/app/src/main/java/com/sunbeam/mrtracker/activity/Homoeopathy.java
@@ -0,0 +1,105 @@
+package com.sunbeam.mrtracker.activity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.sunbeam.mrtracker.R;
+import com.sunbeam.mrtracker.adapter.ProductAdapter;
+import com.sunbeam.mrtracker.model.Product;
+
+import java.util.ArrayList;
+
+public class Homoeopathy extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
+    ArrayList<Product> products = new ArrayList <>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homoeopathy);
+
+        recyclerView = findViewById(R.id.recyclerview);
+
+        adapter = new ProductAdapter(this,products);
+        recyclerView.setAdapter(adapter);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setDisplayShowHomeEnabled(true);
+        bar.setTitle("Homoeopathy");
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("blue")));
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+        {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    protected void onResume() {
+        super.onResume();
+        loadproducts();
+    }
+
+    private void loadproducts(){
+        products.clear();
+
+
+        String url = "http://192.168.2.10:4000/MRlogin/homoeopathy";
+
+        Ion.with(this).load("GET",url).asJsonObject().setCallback(new FutureCallback<JsonObject>() {
+            @Override
+            public void onCompleted(Exception e, JsonObject result) {
+
+                String status = result.get("status").getAsString();
+
+                if(status.equals("success")){
+
+                    JsonArray array = result.get("data").getAsJsonArray();
+
+                    for(int i=0;i < array.size(); i++){
+                        JsonObject object = array.get(i).getAsJsonObject();
+
+                        int id = object.get("id").getAsInt();
+                        String name = object.get("name").getAsString();
+                        int price = object.get("price").getAsInt();
+                        int discount = object.get("discount").getAsInt();
+                        String image = object.get("image").getAsString();
+                        int priceWithDiscount = object.get("priceWithDiscount").getAsInt();
+
+
+                        products.add(new Product(id, name, price, discount, image, priceWithDiscount));
+
+
+                    }
+
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+    }
+
+
+}
